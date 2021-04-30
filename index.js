a2fba2eef83500d15350f499cf54fbe8bab70d7a
Cat Ormerod <catormerod@gmail.com>
2021-04-30 11:31:01 +1000

    questions written

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Enter any installation instructions for your project',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information for your project',
+      },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'Are there any other contributors to this project?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Are tests required?',
+      },
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'which license?',
+        choices: ["MIT", "option2", "option3"],
+      },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
