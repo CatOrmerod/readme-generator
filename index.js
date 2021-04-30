e8717db0395ca0492271d6befd23cb89018bfcb3
Cat Ormerod <catormerod@gmail.com>
2021-04-30 20:05:53 +1000

    api completed

--- index.js
+++ index.js
@@ -1,63 +1,79 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const axios = require("axios");
 const fs = require('fs');
 const util = require('util');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./src/generateMarkdown');
 
 const writeToFile = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a brief description of your project',
-      },
-      {
-        type: 'input',
-        name: 'install',
-        message: 'Enter any installation instructions for your project',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information for your project',
-      },
-      {
-        type: 'input',
-        name: 'contributors',
-        message: 'Are there any other contributors to this project?',
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'Are tests required?',
-      },
-      {
-        type: 'checkbox',
-        name: 'license',
-        message: 'which license?',
-        choices: ["MIT", "option2", "option3"],
-      },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a brief description of your project',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter any installation instructions for your project',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information for your project',
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Are there any other contributors to this project?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Are tests required?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'which license?',
+            choices: ["mit", "gpl-3.0", "apache-2.0"],
+        },
     ]);
 };
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 const init = () => {
     questions()
-      .then((answers) => writeToFile('test.md', generateMarkdown(answers)))
-      .then(() => console.log('Successfully wrote test.md'))
-      .catch((err) => console.error(err));
-  };
+        // TODO: Create a function to write README file
+        // function writeToFile(fileName, data) {}
+        .then(function (answers) {
+            const queryUrl = `https://api.github.com/licenses/${answers.license}`;
+            console.log(answers);
+            axios.get(queryUrl).then(function (res) {
+                
+                const licenseInfo = {
+                    licKey: res.data.key,
+                    licName: res.data.name,
+                    licURL: res.data.html_url,
+                    licBody: res.data.body,
+                };
+                writeToFile('test.md', generateMarkdown({...answers, ...licenseInfo}))
+                    .then(() => console.log('Successfully wrote test.md'))
+                    .catch((err) => console.error(err));
+            })
+            .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
+};
+
 
 // Function call to initialize app
 init();
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,18 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licKey) {
+  if (!licKey)
+    return ""
+  else 
+  return `![badge](https://img.shields.io/badge/license-${licKey}-brightgreen)`
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseInfo) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseInfo) {}
 
 // TODO: Create a function to generate markdown for README
-const generateMarkdown = ({ title, description, install, usage, contributors, test, license }) =>
+const generateMarkdown = ({ title, description, install, usage, contributors, test, licKey, licName, licBody, licURL }) =>
   `# ${title}
+
+  ${renderLicenseBadge(licKey)}
 
   ## Description
   ${description}
@@ -32,7 +40,9 @@ const generateMarkdown = ({ title, description, install, usage, contributors, te
   ${test}
 
   ## License
-  ${license}
+  ${licName}
+  ${licBody}
+  For further information ${licURL}
 
 `;
 
